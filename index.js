c75f69764e84f048317b2601b264d7b0f27eab27
Shimonbrain <Shimonchouhan789@gmail.com>
2021-10-05 15:25:51 +0530

    something update

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ require('express-async-errors')
 const userRouters = require("./user/user.router");
 const app = express();
 const middleware=require('./utils/middleware')
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(morgan('dev'))
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-const PORT = 3000;
 app.use("/users", userRouters);
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)

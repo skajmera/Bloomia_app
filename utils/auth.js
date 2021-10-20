3151606c485508bfffaf79765d45212f13961fce
Subhash Ajmera <subhashajmera2@gmail.com>
2021-10-20 15:30:34 +0530

    process.env updated

--- utils/auth.js
+++ utils/auth.js
@@ -9,13 +9,10 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
         done(null, user);
 });
-// ||process.env.clientID
-// process.env.clientSecret,
-// process.env.callbackURL,
 passport.use(new GoogleStrategy({
-        clientID:"361344985500-l946e4ra5nv3fgf66sgi26msg0euo2no.apps.googleusercontent.com",
-        clientSecret:"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",
-        callbackURL:"https://bloomia.herokuapp.com/users/google/callback",// "/users/google/callback",
+        clientID:process.env.clientID,
+        clientSecret:process.env.clientSecret,
+        callbackURL:process.env.callbackURL,
         passReqToCallback   : true
     },
     function(request, accessToken, refreshToken, profile, done) {
